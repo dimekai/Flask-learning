dbfbb0cef9fc1f6f04cab1830578bdcbd159e2be
dime.kai <jesus_kdm@hotmail.com>
2021-07-15 21:11:59 -0500

    Adding a cookie to get IP user :cookie:

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, make_response, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Gettin IP user who starts the app
+    user_ip = request.remote_addr
+
+    # Redirect a new route
+    response = make_response(redirect('/hello'))
+
+    # Setting a cookie using IP user
+    response.set_cookie('cookie_user_ip', user_ip)
+
+    return response
+
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('cookie_user_ip')
+    return f'Hello, world baby, your IP is {user_ip} using a cookie'
+
+
+
+
+
+
+
+
