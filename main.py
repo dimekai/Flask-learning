83d2bb1139254090e8ff65afa839f9118bb6e431
dime.kai <jesus_kdm@hotmail.com>
2021-07-20 21:52:44 -0500

    render_template with dictionary expanded

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, make_response, redirect
+from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
+
+todos = ['TODO 1', 'TODO 2', 'TODO 3']
 
 @app.route('/')
 def index():
@@ -18,8 +20,12 @@ def index():
 @app.route('/hello')
 def hello():
     user_ip = request.cookies.get('cookie_user_ip')
-    return f'Hello, world baby, your IP is {user_ip} using a cookie'
-
+    context = {
+          'user_ip' : user_ip
+        , 'todos'   : todos
+    }
+    return render_template('hello.html', **context)
+    #return f'Hello, world baby, your IP is {user_ip} using a cookie'
 
 
 
